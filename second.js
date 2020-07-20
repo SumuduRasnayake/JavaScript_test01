9d97b402f61c4e1b5e8e89578d92a09351a12df8
SumuduRasnayake <47654243+SumuduRasnayake@users.noreply.github.com>
2020-07-20 23:52:18 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var btn = document.getElementById("demo");
+    btn.addEventListener("click", myFunction);
+
+    function myFunction() {
+        alert(Math.random());
+        btn.removeEventListener("click", myFunction);
+    }
+};
